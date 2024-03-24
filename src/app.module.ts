e483177d81bce759bdf20ad50ddcefbc4869fb8e
ChangeS99 <luyangpranim@gmail.com>
2024-03-24 21:09:16 +0530

    drizzle started

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
+import { DrizzleModule } from './drizzle/drizzle.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [TasksModule, DrizzleModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,11 +1,14 @@
-import { IsString } from "class-validator";
+import { IsEnum, IsOptional, IsString } from "class-validator";
 import { TaskStatus } from "../task.model";
 
 export class GetTasksFilterDto {
 
+    @IsEnum(TaskStatus)
+    @IsOptional()
     status?: TaskStatus;
 
     @IsString()
+    @IsOptional()
     search?: string;
 
 }
