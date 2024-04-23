a00ad102b95fff1f192e1950c3e416a67c86d94f
ChangeS99 <luyangpranim@gmail.com>
2024-04-23 13:37:37 +0530

    added relation between tasks and users

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -2,8 +2,18 @@
 * Schema file for drizzle
 */
 
+import { relations } from 'drizzle-orm';
 import { text, sqliteTable } from 'drizzle-orm/sqlite-core';
 import { v4 as uuid } from "uuid"
+
+
+
+
+export const users = sqliteTable("users", {
+    id: text("id").primaryKey().$defaultFn(() => uuid()),
+    username: text("username").unique().notNull(),
+    password: text("password").notNull()
+})
 
 export const tasks = sqliteTable('tasks', {
     id: text("id").primaryKey().$defaultFn(() => uuid()),
@@ -11,12 +21,23 @@ export const tasks = sqliteTable('tasks', {
     description: text('description').notNull(),
     status: text("status", {
         enum: ["OPEN", "IN_PROGRESS", "DONE"]
-    }).default("OPEN")
+    }).default("OPEN"),
+    authodId: text('authod_id').references(() => users.id, { onDelete: 'cascade' })
 });
 
+// set up relation of users
+export const usersRelations = relations(users, ({ many }) => {
+    return {
+        tasks: many(tasks)
+    }
+})
 
-export const users = sqliteTable("users", {
-    id: text("id").primaryKey().$defaultFn(() => uuid()),
-    username: text("username").unique().notNull(),
-    password: text("password").notNull()
+// set up relation of posts
+export const tasksRelations = relations(tasks, ({ one }) => {
+    return {
+        user: one(users, {
+            fields: [tasks.authodId],
+            references: [users.id]
+        })
+    }
 })
